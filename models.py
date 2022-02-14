4ed83516f1dbf70ef4db90813854d7a461870cf5
SodiumN1trate <renarsgausins21@gmail.com>
2022-02-14 16:01:06 +0200

    es hz

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from django import views
 from settings import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -59,6 +60,21 @@ class Trip(db.Model):
     def get_agency_from_id(self):
         return Agency.query.filter(Agency.id == self.agency_id).first()
     
+    def serialize(self):
+        return {
+            "id": self.id,
+            "agency_id": self.agency_id,
+            "country_from_id": self.country_from,
+            "country_to_id": self.country_to,
+            "country_from": "",
+            "country_to": "",
+            "date_from": self.date_from,
+            "date_to": self.date_to,
+            "description": self.description,
+            "cost": self.cost,
+            "ticket_amount": self.ticket_amount,
+            "views": self.views
+            }
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), nullable=False, unique=True)
